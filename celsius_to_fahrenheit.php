a79911c306a9410deb0981ee20dd3343c807e42a
musriful-web <musriful.web@gmail.com>
2024-07-08 20:46:00 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,4 @@
+<?php
+$celcius = 32;
+$farenhit = ((9/5)*$celcius+32);
+printf("%s°C = %.2f°F",$celcius,$farenhit);
---
+++ largest_number.php
@@ -0,0 +1,16 @@
+<?php
+echo "Try programiz.pro";
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+$largeNumber;
+if ($num1>$num2){
+    $largeNumber = $num1;
+}
+else {
+    $largeNumber = $num2;
+}
+if ($largeNumber<$num3){
+    $largeNumber = $num3;
+}
+echo "The largest Number is" .$largeNumber;
